83f5da677d4e813d00a6cf9fb0a073523d58e404
louispy <louisleslie98@gmail.com>
2025-04-26 14:38:16 +0700

    Add elasticsearch module

--- src/consumer/consumer.service.ts
+++ src/consumer/consumer.service.ts
@@ -1,20 +1,22 @@
 import { Inject, Injectable } from '@nestjs/common';
+import { ElasticsearchService } from '@nestjs/elasticsearch';
 import { KafkaContext } from '@nestjs/microservices';
+import { instanceToPlain, plainToInstance } from 'class-transformer';
+import { validateSync } from 'class-validator';
 
+import { ELASTICSEARCH_MESSAGES_INDEX } from '../elasticsearch/elasticsearch.module';
 import { KafkaTopic } from '../kafka/interfaces/kafka.interface';
 import { KAFKA_TOPIC } from '../kafka/interfaces/kafka.tokens.interface';
 import { BaseKafkaConsumerService } from '../kafka/kafka.consumer.service';
 import { IndexMessageMessageDto } from './dto/consumer.message.dto';
-import {
-  ClassTransformer,
-  plainToClass,
-  plainToInstance,
-} from 'class-transformer';
-import { validateSync } from 'class-validator';
 
 @Injectable()
 export class ConsumerService extends BaseKafkaConsumerService {
-  constructor(@Inject(KAFKA_TOPIC) private readonly kafkaTopic: KafkaTopic) {
+  constructor(
+    @Inject(KAFKA_TOPIC) private readonly kafkaTopic: KafkaTopic,
+    @Inject(ELASTICSEARCH_MESSAGES_INDEX) private readonly index: string,
+    private readonly elasticsearchService: ElasticsearchService,
+  ) {
     super();
   }
 
@@ -29,17 +31,25 @@ export class ConsumerService extends BaseKafkaConsumerService {
   }
 
   private async indexMessage(data: any): Promise<void> {
-    const msg: IndexMessageMessageDto = plainToInstance(
-      IndexMessageMessageDto,
-      data,
-    );
+    try {
+      const msg: IndexMessageMessageDto = plainToInstance(
+        IndexMessageMessageDto,
+        data,
+      );
 
-    const errors = validateSync(msg);
-    if (errors.length > 0) {
-      throw new Error(`Payload validation failed: ${JSON.stringify(errors)}`);
+      const errors = validateSync(msg);
+      if (errors.length > 0) {
+        throw new Error(`Payload validation failed: ${JSON.stringify(errors)}`);
+      }
+
+      await this.elasticsearchService.index({
+        index: this.index,
+        body: instanceToPlain(msg),
+      });
+      console.log('success consume message!!');
+      console.log(msg);
+    } catch (err) {
+      console.error(err);
     }
-
-    console.log('success consume message!!');
-    console.log(msg);
   }
 }
