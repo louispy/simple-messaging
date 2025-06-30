e5e516b6c620a9816dbf2d01aebb3b76ab8a8249
louispy <spy.tech23@gmail.com>
2025-06-30 15:28:47 +0700

    Add applogger and requestcontext middlewares to app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,14 +1,16 @@
 import 'reflect-metadata';
 
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
+import { AppLoggerMiddleware } from './common/middlewares/app.logger';
+import { RequestContextMiddleware } from './common/middlewares/request.context';
+import { ConversationsModule } from './conversations/conversations.module';
 import { DatabaseModule } from './database/database.module';
 import { LoggerModule } from './logger/logger.module';
-import { ConversationsModule } from './conversations/conversations.module';
 
 @Module({
   imports: [
@@ -21,4 +23,9 @@ import { ConversationsModule } from './conversations/conversations.module';
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(AppLoggerMiddleware).forRoutes('*');
+    consumer.apply(RequestContextMiddleware).forRoutes('*');
+  }
+}
